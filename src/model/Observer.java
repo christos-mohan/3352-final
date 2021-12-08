ac08209bf537425af24ffb3ab3f66f04dc42e5d7
ElliotLam <elam222@uwo.ca>
2021-12-08 02:25:24 -0500

    testing

--- src/model/Observer.java
+++ src/model/Observer.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 
+//testing for git push
 // Abstract class used for updating subjects (and the pages are subsets of the subject)
 public abstract class Observer {
 
